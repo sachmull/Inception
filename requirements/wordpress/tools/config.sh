dc61507069f57c00a0ce7ef7d1041b536abae298
xhazm <lars.pfleiderfb@gmail.com>
2022-07-09 12:04:12 -0500

    added: wordpress environment now working. wp site also available

--- requirements/wordpress/tools/config.sh
+++ requirements/wordpress/tools/config.sh
@@ -2,21 +2,12 @@
 
 cd /var/www/html/
 
-wp core config	--dbhost=$DB_HOST \
-				--dbname=$DB_NAME \
-				--dbuser=$DB_USER \
-				--dbpass=$DB_PASSWORD \
+wp core install --title=${WP_TITLE} \
+				--admin_user=${WP_ADMIN_USER} \
+				--admin_password=${WP_ADMIN_PASSWORD} \
+				--admin_email=${WP_ADMIN_MAIL} \
+				--url=${WP_URL} \
 				--allow-root
-
-wp core install --title=$WP_TITLE \
-				--admin_user=$WP_ADMIN_USER \
-				--admin_password=$WP_ADMIN_PASSWORD \
-				--admin_email=$WP_ADMIN_MAIL \
-				--url=$WP_URL \
-				--allow-root
-
-wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
-cd -
 
 # php-fpm listening for fastcgi request || -F to stay in foreground and dont deamonize
 php-fpm7.3 -F
