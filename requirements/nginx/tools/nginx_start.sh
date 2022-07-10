386d5eb89ed713cfe9d6bedfdd6c6549e7e995e9
xhazm <lars.pfleiderfb@gmail.com>
2022-07-10 15:52:10 -0500

    added: small nginx docker rework | tocomplete: mariadb setup through sql file - wrong syntax so far

--- requirements/nginx/tools/nginx_start.sh
+++ requirements/nginx/tools/nginx_start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ln -s /etc/nginx/sites-available/lpfleide.42.fr /etc/nginx/sites-enabled/
+
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 echo "Nginx: setting up ssl ...";
 openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -sha256 -subj "/CN=lpfleide.42.fr";
