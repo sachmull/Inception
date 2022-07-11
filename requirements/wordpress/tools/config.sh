a33443124119a11cb7e011fe98a8be9a953aad52
sachmull <s.acosta.sal@gmail.com>
2022-07-11 10:14:18 -0400

    Enclosed wordpress db and user creation in if statements

--- requirements/wordpress/tools/config.sh
+++ requirements/wordpress/tools/config.sh
@@ -2,11 +2,13 @@
 
 cd /var/www/html/
 
+if [ ! -f /var/www/html/wp-config.php ]; then
 wp config create	--dbhost=${DB_HOST} \
 					--dbname=${DB_NAME} \
 					--dbuser=${DB_USER} \
 					--dbpass=${DB_PASSWORD} \
 					--allow-root
+fi
 
 wp core install --title=${WP_TITLE} \
 				--admin_user=${WP_ADMIN_USER} \
@@ -15,7 +17,10 @@ wp core install --title=${WP_TITLE} \
 				--url=${WP_URL} \
 				--allow-root
 
+wp user get user1 --allow-root --path='/var/www/html/' --field=user_login
+if  [ $? -eq 1 ] ; then
 wp user create ${WP_USER} ${WP_USER_MAIL} --role=author --user_pass=${WP_USER_PASSWORD} --allow-root
+fi 
 cd -
 
 # php-fpm listening for fastcgi request || -F to stay in foreground and dont deamonize
